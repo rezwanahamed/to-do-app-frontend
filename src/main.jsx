43e1624038cedd2829fc1cf4b765295ce1d58ea6
rezwanahamed <rezwanahamed81@gmail.com>
2025-02-25 15:22:09 +0600

    login and register page added

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import LandingPage from "./LandingPage";
-import About from "./pages/Login";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>,
 );
